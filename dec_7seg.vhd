e4585a8ada5b85f205f8bad908bdf86499b7c009
MiguelMoncayo0730 <miguelmoncayo@unicauca.edu.co>
2026-09-22 22:50:46 -0500

    Add files via upload

--- dec_7seg.vhd
+++ dec_7seg.vhd
@@ -12,15 +12,15 @@ architecture dec_7seg_arch of dec_7seg is
 begin
     -- Asignación con with-select y mapeo
     with bcd select
-        seg_out <= "1111110" when "0000", -- Muestra 0
-                   "0110000" when "0001", -- Muestra 1
-                   "1101101" when "0010", -- Muestra 2
-                   "1111001" when "0011", -- Muestra 3
-                   "0110011" when "0100", -- Muestra 4
-                   "1011011" when "0101", -- Muestra 5
-                   "1011111" when "0110", -- Muestra 6
-                   "1110000" when "0111", -- Muestra 7
-                   "1111111" when "1000", -- Muestra 8
-                   "1111011" when "1001", -- Muestra 9
+        seg_out <= "1111110" when "0000", -- 0
+                   "0110000" when "0001", -- 1
+                   "1101101" when "0010", -- 2
+                   "1111001" when "0011", -- 3
+                   "0110011" when "0100", -- 4
+                   "1011011" when "0101", -- 5
+                   "1011111" when "0110", -- 6
+                   "1110000" when "0111", -- 7
+                   "1111111" when "1000", -- 8
+                   "1111011" when "1001", -- 9
                    "0000000" when others; -- apaga en otros casos
 end architecture;
